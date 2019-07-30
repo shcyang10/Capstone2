61225725b114990dfc0c11d7216d7189ffd14ca1
sean <seanhcyang10@gmail.com>
2019-07-29 21:57:19 -0400

    second commit

--- src/co/grandcircus/Task.java
+++ src/co/grandcircus/Task.java
@@ -6,47 +6,67 @@ public class Task {
 
 	private String name;
 	private String description;
-	private LocalDate dueDate;
+	private String dueDate;
 	private String completed;
 	
+	
+
 	public Task() {
 		completed = "false";
+		
 	}
-	public Task(String name, String description, LocalDate dueDate) {
+
+	public Task(String name, String description, String dueDate) {
+		super();
 		this.name = name;
 		this.description = description;
 		this.dueDate = dueDate;
 		completed = "false";
 	}
-	public void setName(String name) {
-		this.name = name;
-	}
-	public void setDescription(String description) {
-		this.description = description;
-	}
-	public void setDueDate(LocalDate dueDate) {
-		this.dueDate = dueDate;
-	}
-	public void setCompleted(String completed) {
-		this.completed = completed;
-	}
+
 	public String getName() {
 		return name;
 	}
+
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+
 	public String getDescription() {
 		return description;
 	}
-	public LocalDate getDueDate() {
+
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+
+	public String getDueDate() {
 		return dueDate;
 	}
-	public String getCompleted() {
+
+
+	public void setDueDate(String dueDate) {
+		this.dueDate = dueDate;
+	}
+
+
+	public String isCompleted() {
 		return completed;
 	}
+
+
+	public void setCompleted(String completed) {
+		this.completed = completed;
+	}
+
 	@Override
 	public String toString() {
-		return "Name=" + name + ", Description=" + description + ", DueDate=" + dueDate + ", Completed="
-				+ completed;
+		String myFormat = "Name: %30-s Description %30-s Due Date %15-s Done? %10-s";
+		return String.format(myFormat, name, description, dueDate, completed);
 	}
 	
-
 }
